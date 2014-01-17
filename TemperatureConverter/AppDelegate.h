9a1b0a11bd5f375f15a468831421b2013773437d
Chix <bbhati@bbhati-ltm99.hsd1.ca.comcast.net>
2014-01-16 22:31:55 -0800

    Initial Commit

---
+++ TemperatureConverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TemperatureConverter
+//
+//  Created by Chix on 1/16/14.
+//  Copyright (c) 2014 Bhagyashree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TemperatureConverterTests/TemperatureConverterTests.h
@@ -0,0 +1,13 @@
+//
+//  TemperatureConverterTests.h
+//  TemperatureConverterTests
+//
+//  Created by Chix on 1/16/14.
+//  Copyright (c) 2014 Bhagyashree. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TemperatureConverterTests : SenTestCase
+
+@end
